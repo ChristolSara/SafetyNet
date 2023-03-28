d4186f4f128a6c358cf5a073942ab6b7c101a3de
SaraaMiss <sara.kaouar.christol@gmail.com>
2023-03-28 10:53:00 +0200

    2 commit medicalRecord  model ,controler

--- SafetyNet/src/main/java/com/SafetyNet/contoller/MedicalRecordController.java
+++ SafetyNet/src/main/java/com/SafetyNet/contoller/MedicalRecordController.java
@@ -1,9 +1,27 @@
 package com.SafetyNet.contoller;
 
+import com.SafetyNet.model.MedicalRecord;
 import com.SafetyNet.service.MedicalRecordService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MedicalRecordController {
     private MedicalRecordService medicalRecordService;
+    @PostMapping(value="/medicalRecord")
+    public MedicalRecord ajouterMedicalRecord(MedicalRecord medicalRecord){
+        return medicalRecordService.save(medicalRecord);
+    }
+    @PutMapping(value = "/medicalRecord")
+    public MedicalRecord miseAjourMedicalRecord(MedicalRecord medicalRecord){
+        return medicalRecordService.update(medicalRecord);
+    }
+
+    @DeleteMapping(value = "/medicalRecord")
+    public void supprimermedicalRecord(MedicalRecord medicalRecord){
+        medicalRecordService.delete(medicalRecord);
+    }
+
 }
--- SafetyNet/src/main/java/com/SafetyNet/service/MedicalRecordService.java
+++ SafetyNet/src/main/java/com/SafetyNet/service/MedicalRecordService.java
@@ -1,7 +1,18 @@
 package com.SafetyNet.service;
 
+import com.SafetyNet.model.MedicalRecord;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MedicalRecordService {
+    public MedicalRecord save(MedicalRecord medicalRecord) {
+        return medicalRecord;
+    }
+
+    public MedicalRecord update(MedicalRecord medicalRecord) {
+        return medicalRecord;
+    }
+
+    public void delete(MedicalRecord medicalRecord) {
+    }
 }
